2f122e235357645c120f4a644a9912f3216caeed
Aviva Weinbaum <aw3156@barnard.edu>
2021-02-21 14:03:21 -0500

    adding ast

---
+++ ast.ml
@@ -0,0 +1,44 @@
+(* AST for Reptile *)
+(* Aviva Weinbaum, Lindsey Weiskopf, Hariti Patel, Aileen Cano *)
+
+type operator = Add | Sub | Mul | Div | Equal | Neq | Less | Greater | Geq | 
+          And | Or | Exp | Mod
+
+type unoperator = Not | Neg
+type incoperator = Incr | Decr
+
+type typ = Int | Bool | Float | Void | String | Rgb | Canvas | Pointer | 
+          File | Array of typ * int
+
+type bind = typ * string
+
+type expr =
+    Binop of expr * operator * expr
+  | Unop of unoperator * expr
+  | Inc of expr * incoperator
+  | Literal of int
+  | Fliteral of string
+  | BoolLit of bool
+  | Id of string
+  | String of string
+  | Assign of string * expr
+  | Call of string * expr list
+  | ArrayAccess of string * expr
+  | ArrayLit of expr list         (* question *)
+  | Noexpr
+
+type stmt = 
+    Expr of expr
+  | Return of expr
+  | Block of stmt list
+  | If of expr * stmt * stmt
+  | For of expr * expr * expr * stmt 
+  | While of expr * stmt
+
+  type func_decl = {
+    typ : typ;
+    fname : string;
+    formals : bind list;
+    locals : bind list;
+    body : stmt list;
+  }
