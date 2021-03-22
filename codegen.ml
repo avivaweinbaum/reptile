57a53363c5a9f6d0d164971742d31260a2aa1efa
Lindsey Weiskopf <ltweiskopf@gmail.com>
2021-03-21 23:35:06 -0400

    started codegen

---
+++ codegen.ml
@@ -0,0 +1,108 @@
+module L = Llvm
+module A = Ast
+open Sast
+
+module StringMap = Map.Make(String)
+
+(* translate : Sast.program -> Llvm.module *)
+let translate (globals, functions) =
+  let context    = L.global_context () in
+
+  (* Create the LLVM compilation module into which
+     we will generate code *)
+  let the_module = L.create_module context "Reptile" in
+
+(* Get types from the context *)
+  let i32_t      = L.i32_type    context
+  and i8_t       = L.i8_type     context
+  and i1_t       = L.i1_type     context
+  and float_t    = L.double_type context
+  and void_t     = L.void_type   context 
+  and string_t   = L.pointer_type (L.i8_type context)
+  and rgb_t      = L.pointer_type (L.i8_type context)
+  and canvas_t   = L.pointer_type (L.i8_type context)
+  and pointer_t  = (* TO DO *)
+  and file_t     = L.pointer_type (L.i8_type context)
+  and array_t    = L.array_type context
+
+in
+
+(* Return the LLVM type for a MicroC type *)
+  let ltype_of_typ = function
+      A.Int   -> i32_t
+    | A.Bool  -> i1_t
+    | A.Float -> float_t
+    | A.Void  -> void_t
+    | A.String -> string_t
+    | A.rgb -> rgb_t
+    | A.canvas -> canvas_t
+    | A.pointer -> pointer_t
+    | A.file -> file_t
+    | A.array -> array_t
+  in
+
+    let global_vars : L.llvalue StringMap.t =
+    let global_var m (t, n) =
+      let init = match t with
+          A.Float -> L.const_float (ltype_of_typ t) 0.0
+        | _ -> L.const_int (ltype_of_typ t) 0
+      in StringMap.add n (L.define_global n init the_module) m in
+    List.fold_left global_var StringMap.empty globals in
+
+  let printf_t : L.lltype =
+      L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
+  let printf_func : L.llvalue =
+      L.declare_function "printf" printf_t the_module in
+
+  let printbig_t : L.lltype =
+      L.function_type i32_t [| i32_t |] in
+  let printbig_func : L.llvalue =
+      L.declare_function "printbig" printbig_t the_module in
+
+(* Define each function (arguments and return type) so we can
+     call it even before we've created its body *)
+  let function_decls : (L.llvalue * sfunc_decl) StringMap.t =
+    let function_decl m fdecl =
+      let name = fdecl.sfname
+      and formal_types =
+        Array.of_list (List.map (fun (t,_) -> ltype_of_typ t) fdecl.sformals)
+      in let ftype = L.function_type (ltype_of_typ fdecl.styp) formal_types in
+      StringMap.add name (L.define_function name ftype the_module, fdecl) m in
+    List.fold_left function_decl StringMap.empty functions in
+
+  (* Fill in the body of the given function *)
+  let build_function_body fdecl =
+    let (the_function, _) = StringMap.find fdecl.sfname function_decls in
+    let builder = L.builder_at_end context (L.entry_block the_function) in
+
+    let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder
+    and float_format_str = L.build_global_stringptr "%g\n" "fmt" builder 
+    and str_format_str = L.build_global_stringptr "%s\n" "fmt" building in
+
+ (* Construct the function's "locals": formal arguments and locally
+       declared variables.  Allocate each on the stack, initialize their
+       value, if appropriate, and remember their values in the "locals" map *)
+    let local_vars =
+      let add_formal m (t, n) p =
+        L.set_value_name n p;
+        let local = L.build_alloca (ltype_of_typ t) n builder in
+        ignore (L.build_store p local builder);
+        StringMap.add n local m
+
+ (* Allocate space for any locally declared variables and add the
+       * resulting registers to our map *)
+      and add_local m (t, n) =
+        let local_var = L.build_alloca (ltype_of_typ t) n builder
+        in StringMap.add n local_var m
+      in
+ let formals = List.fold_left2 add_formal StringMap.empty fdecl.sformals
+          (Array.to_list (L.params the_function)) in
+      List.fold_left add_local formals fdecl.slocals
+    in
+
+    (* Return the value for a variable or formal argument.
+       Check local names first, then global names *)
+    let lookup n = try StringMap.find n local_vars
+                   with Not_found -> StringMap.find n global_vars
+    in
+
